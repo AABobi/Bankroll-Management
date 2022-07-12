6cf1e4426dbf0a18a8cd6003e3c4cc3d616ac896
Radosław Kopeć <radoslawkopec93@gmail.com>
2022-07-12 20:53:11 +0200

    First commit

---
+++ src/main/java/radoslaw/kopec/BankrollManagement/BankrollManagementApplication.java
@@ -0,0 +1,13 @@
+package radoslaw.kopec.BankrollManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankrollManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankrollManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/radoslaw/kopec/BankrollManagement/BankrollManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package radoslaw.kopec.BankrollManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankrollManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
